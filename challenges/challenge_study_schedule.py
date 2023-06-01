ea49920d010af8cd2967ee8ae428e40bfb5e92af
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-01 17:15:07 -0300

    Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    count = 0
+    for period in permanence_period:
+        start_time, end_time = period
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+        if start_time <= target_time <= end_time:
+            count += 1
+    return count
