abfce450166f51b4389847eece926ab12455c93f
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-01 18:07:17 -0300

    Requisito 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+    if any(not isinstance(num, int) or num < 0 for num in nums):
+        return False
+    nums.sort()
+    for index in range(1, len(nums)):
+        if (nums[index] == nums[index - 1]):
+            return nums[index]
+    return False
